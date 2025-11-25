8715a02167d501be05c89e4c5b1e5e87320869ef
Rohan Tennety <rtennety@gmail.com>
2025-11-24 18:59:11 -0800

    Revert password back to autonomous

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // ========== PASSWORD PROTECTION ==========
-    const SITE_PASSWORD = 'autonomous4d';
+    const SITE_PASSWORD = 'autonomous';
     
     // Show main content and hide password overlay
     function showMainContent() {
